4c7497605874b60abc47b5898b0b993be5923938
ImogenReading <76921782+ImogenReading@users.noreply.github.com>
2021-03-21 01:51:01 +0000

    Adding City Element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Search from './Search';
+import City from './City';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       
         <Search/>   
+        <City/>
   
     </div>
   );

1dd8154d95987e4c09cec2b93f4dbfb75ad18aa5
ImogenReading <76921782+ImogenReading@users.noreply.github.com>
2021-03-21 02:07:13 +0000

    Removing Empty href to hopefully remedy Netifly hosting issue

--- src/Weather.js
+++ src/Weather.js
@@ -4,24 +4,21 @@ import React from "react";
 export default function Weather() {
   return (
     <div>
-      {" "}
+    
       <h3 id="weather-description">Sunshine and light wind </h3>
-      <img
-        src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-        id="weather-icon"
-      />
+      
       <br />
       <span className="temperature" id="temperature">
         10
       </span>
       <span className="units">
-        <a href="/" id="celsius-link" className="active">
+        <span id="celsius-link" className="active">
           °C
-        </a>{" "}
+        </span>
         |
-        <a href="/" id="fahrenheit-link">
+        <span id="fahrenheit-link">
           °F
-        </a>
+        </span>
       </span>
       <br />
       <span className="humidity" id="humidity">
